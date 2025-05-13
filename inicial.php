abac881be923313f8399a478b716ca597e6415d3
Aluno <aluno@etecjk.local>
2025-05-12 22:12:38 -0300

    primeiro commit site

---
+++ inicial.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <link rel="stylesheet" href="inicial.css">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Promotores de venda</title>
+
+    <style>body { font-family: Arial, sans-serif; text-align: center; }
+        header { background-color: #007BFF; color: white; padding: 20px; }
+        section { margin: 20px; padding: 20px; border: 1px solid #ccc; }
+        footer { background-color: #f1f1f1; padding: 10px; }
+    </style>
+</head>
+
+<body>
+
+    
+
+    <h1>Bem-Vindo!</h1>
+
+    <a class="promotoria" href="file:///C:/Users/aluno/Desktop/PaginaInicialPHP/promotoria.html">Clique AQUI para acessar promotores</a>
+<br>
+    <a class="administrador" href="file:///C:/Users/aluno/Desktop/PaginaInicialPHP/administrador.html">Clique AQUI para acessar administração</a>
+</body>
+</html>
---
+++ promotoria.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <link rel="stylesheet" href="promotoria.css">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Promotoria</title>
+</head>
+<body>
+    <H1>PROMOTORES</H1>
+</body>
+</html>
