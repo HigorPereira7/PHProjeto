8878df6ad9aacb9ea6cadffb1e2ac7e630638e45
Aluno <aluno@etecjk.local>
2025-05-19 22:06:20 -0300

    pagina inicial

--- inicial.php
+++ inicial.php
@@ -1,26 +1,25 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-    <link rel="stylesheet" href="inicial.css">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Promotores de venda</title>
-
-    <style>body { font-family: Arial, sans-serif; text-align: center; }
-        header { background-color: #007BFF; color: white; padding: 20px; }
-        section { margin: 20px; padding: 20px; border: 1px solid #ccc; }
-        footer { background-color: #f1f1f1; padding: 10px; }
-    </style>
+    <link rel="stylesheet" href="inicial.css">
 </head>
 
 <body>
+    <header>
+        <h1>Bem-Vindo!</h1>
+    </header>
+
+    <a class="promotoria" href="promotoria.php">
+        <button type="button">Clique AQUI para acessar promotoria</button>
+    </a>
+    <br>
+    <a class="administrador" href="administrador.php">
+        <button type="button">Clique AQUI para acessar administração</button>
+    </a>
 
     
-
-    <h1>Bem-Vindo!</h1>
-
-    <a class="promotoria" href="file:///C:/Users/aluno/Desktop/PaginaInicialPHP/promotoria.html">Clique AQUI para acessar promotores</a>
-<br>
-    <a class="administrador" href="file:///C:/Users/aluno/Desktop/PaginaInicialPHP/administrador.html">Clique AQUI para acessar administração</a>
 </body>
 </html>
